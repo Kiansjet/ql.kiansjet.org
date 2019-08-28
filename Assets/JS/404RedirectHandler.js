240722d03e674269272f1330ffcbe7cf364b7ab9
Kian M <kianmahmoudi@outlook.com>
2019-08-27 23:57:46 -0700

    Initial test

---
+++ Assets/JS/404RedirectHandler.js
@@ -0,0 +1,29 @@
+// Runs on the siteRoot/404.html proxy page and handles all redirection.
+
+let pathname = document.location.pathname
+let redirectText = document.getElementById('redirectText')
+
+if (!document.location.host) {
+	// Page is loaded locally. Forget everything else.
+	redirectText.innerHTML = 'Page loaded locally. Function disabled.'
+} else if (pathname.endsWith('404.html')) {
+	// Page was loaded directly, display rejection text.
+	redirectText.innerHTML = 'You directly navigated to this page. Nothing will happen.'
+} else {
+	// 404 page has been loaded as a proxy for a missing page.
+
+	// Remove the first character from the pathname as its always a /
+	let quickLink = pathname.substring(1)
+
+	// Parse QuickLinks
+	let quickLinksArray = JSON.parse(QuickLinks)
+
+	// Find valid URL match if any
+	let quickLinkResult = quickLinksArray[quickLink]
+
+	if (quickLinkResult) {
+		document.location = quickLinkResult
+	} else {
+		redirectText.innerHTML = `${quickLink} is not a valid QuickLink.`
+	}
+}
