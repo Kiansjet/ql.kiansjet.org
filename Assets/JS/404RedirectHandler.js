829629f1c7345dc8c8884bd7abb25de1c1bf174b
Kian M <kianmahmoudi@outlook.com>
2019-08-29 08:02:33 -0700

    jQuery upgrade

--- Assets/JS/404RedirectHandler.js
+++ Assets/JS/404RedirectHandler.js
@@ -1,29 +1,35 @@
 // Runs on the siteRoot/404.html proxy page and handles all redirection.
 
-let pathname = document.location.pathname
-let redirectText = document.getElementById('redirectText')
+$(document).ready(function() {
+	let pathname = document.location.pathname
+	let redirectText = document.getElementById('redirectText')
 
-if (!document.location.host) {
-	// Page is loaded locally. Forget everything else.
-	redirectText.innerHTML = 'Page loaded locally. Function disabled.'
-} else if (pathname.endsWith('404.html')) {
-	// Page was loaded directly, display rejection text.
-	redirectText.innerHTML = 'You directly navigated to this page. Nothing will happen.'
-} else {
-	// 404 page has been loaded as a proxy for a missing page.
-
-	// Remove the first character from the pathname as its always a /
-	let quickLink = pathname.substring(1)
-
-	// Parse QuickLinks
-	let quickLinksArray = JSON.parse(QuickLinks)
-
-	// Find valid URL match if any
-	let quickLinkResult = quickLinksArray[quickLink]
-
-	if (quickLinkResult) {
-		document.location = quickLinkResult
+	if (!document.location.host) {
+		// Page is loaded locally. Forget everything else.
+		redirectText.innerHTML = 'Page loaded locally. Function disabled.'
+	} else if (pathname.endsWith('404.html')) {
+		// Page was loaded directly, display rejection text.
+		redirectText.innerHTML = 'You directly navigated to this page. Nothing will happen.'
 	} else {
-		redirectText.innerHTML = `${quickLink} is not a valid QuickLink.`
+		// 404 page has been loaded as a proxy for a missing page.
+	
+		// Remove the first character from the pathname as its always a /
+		let quickLink = pathname.substring(1)
+	
+		// Parse QuickLinks
+		$.getJSON('Assets/JSON/QuickLinks.json',function(quickLinksArray) {
+			console.log(`JSON array is ${quickLinksArray}`)
+
+			let quickLinkResult = quickLinksArray[quickLink]
+
+			if (quickLinkResult) {
+				document.location = quickLinkResult
+			} else {
+				redirectText.innerHTML = `${quickLink} is not a valid QuickLink.`
+			}
+		}).catch(function(err) {
+			redirectText.innerHTML(`QuickLink JSON file failed to load:\n${err}`)
+		})
+		//let quickLinksArray = JSON.parse(QuickLinks)
 	}
-}
+})
