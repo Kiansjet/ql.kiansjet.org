1a66c283af8443fd1fcbe8f4d4434958f2e0a629
Kian M <kianmahmoudi@outlook.com>
2020-05-15 22:53:07 -0700

    Simplified redirect logic

--- Assets/JS/404RedirectHandler.js
+++ Assets/JS/404RedirectHandler.js
@@ -4,16 +4,15 @@ $(document).ready(function() {
 	let pathname = document.location.pathname
 
 	let redirectText = document.getElementById('redirectText')
-	let pageTitle = document.getElementById('pageTitle')
 
 	if (!document.location.host) {
 		// Page is loaded locally. Forget everything else.
 		redirectText.innerHTML = 'Page loaded locally. Function disabled.'
-		pageTitle.innerHTML = 'Redirect failed.'
-	} else if (pathname.endsWith('404.html')) {
+		document.title = 'Redirect failed.'
+	} else if (pathname == "404.html") {
 		// Page was loaded directly, display rejection text.
 		redirectText.innerHTML = 'You directly navigated to this page. Nothing will happen.'
-		pageTitle.innerHTML = 'Redirect failed.'
+		document.title = 'Redirect failed.'
 	} else {
 		// 404 page has been loaded as a proxy for a missing page.
 	
@@ -28,11 +27,11 @@ $(document).ready(function() {
 				document.location = quickLinkResult
 			} else {
 				redirectText.innerHTML = `\"${quickLink}\" is not a valid QuickLink. QuickLinks are case-sensetive.`
-				pageTitle.innerHTML = 'Redirect failed.'
+				document.title = 'Redirect failed.'
 			}
 		}).catch(function(err) {
 			redirectText.innerHTML(`QuickLink JSON file failed to load:\n${err}`)
-			pageTitle.innerHTML = 'Redirect failed.'
+			document.title = 'Redirect failed.'
 		})
 	}
 })
