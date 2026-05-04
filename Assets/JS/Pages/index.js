5b5750f2f8f3239b22edca64653ea0c59fd951a6
Kian M. <13376313+Kiansjet@users.noreply.github.com>
2026-05-04 16:40:12 -0700

    More xss protection, touched up index.js

--- Assets/JS/Pages/index.js
+++ Assets/JS/Pages/index.js
@@ -1,16 +1,23 @@
 // Runs on siteRoot/index.html
 
 // Handle compatibility QuickLink mode
-let firstURLSearchParamKey = new URLSearchParams(document.location.search).keys().next().value
+let firstURLSearchParamKey = new URLSearchParams(document.location.search)
+	.keys().next().value
+
 if (firstURLSearchParamKey) {
-    document.location = document.location.origin + "/" + firstURLSearchParamKey
+
+	document.location = document.location.origin + "/" +
+		encodeURIComponent(firstURLSearchParamKey)
 }
 
 document.addEventListener("readystatechange", function() {
-    // Handle QuickLink search box
-    let quickLinkSearchBox = document.getElementById("quickLinkSearchBox")
 
-    quickLinkSearchBox.addEventListener("search", function() {
-        document.location = document.location.origin + "/" + quickLinkSearchBox.value
-    })
+	// Handle QuickLink search box
+	let quickLinkSearchBox = document.getElementById("quickLinkSearchBox")
+
+	quickLinkSearchBox.addEventListener("search", function() {
+
+		document.location = document.location.origin + "/" +
+			encodeURIComponent(quickLinkSearchBox.value)
+	})
 })
